9fa85edd8c2665fef2cd0bdc2abb583f7fdceaac
davidplay7 <72691307+davidplay7@users.noreply.github.com>
2023-09-03 17:45:03 -0300

    primeiros passos

--- projetoreact/src/App.js
+++ projetoreact/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import ppp from './ppp.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={ppp} className="App-logo" alt="logo" />
       
     </header>
   </div>
